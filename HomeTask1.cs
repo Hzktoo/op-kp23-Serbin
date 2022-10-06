b1eb436ab72c598cbeafbcb5961a19fa64a6e67e
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-10-06 20:58:15 +0300

    Add files via upload

---
+++ HomeTask1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HomeworkTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            Console.WriteLine("Enter value for a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for c");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for d");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    if (a < d)
+                    {
+                        Console.WriteLine("The least number is:" + a);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The least number is:" + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("The least number is:" + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The least number is:" + d);
+                    }
+                }
+            }
+            else
+            {
+                if (b < c)
+                {
+                    if (b < d)
+                    {
+                        Console.WriteLine("The least number is:" + b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The least number is:" + d);
+                    }
+                }
+                else
+                {
+                    if (c < d)
+                    {
+                        Console.WriteLine("The least number is:" + c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The least number:" + d);
+                    }
+                }
+            }
+        }
+    }
+}
