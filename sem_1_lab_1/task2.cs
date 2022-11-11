026233d132585491cc3ad5d8614accc07894509d
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-11 20:17:26 +0200

    task_2_first_commit

---
+++ sem_1_lab_1/task2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace task2
+{
+    class Programm
+    {
+        public static void Main(String[] args)
+        {
+            int n;
+            Console.WriteLine("n is not prime");
+            Console.WriteLine("number n is not prime");
+        }
+    }
+}
