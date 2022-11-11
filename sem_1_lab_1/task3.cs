ca4de0462638f4ef4e338e2a5126df6d766cfb46
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-11 22:36:02 +0200

    task_3_second_commit

--- sem_1_lab_1/task3.cs
+++ sem_1_lab_1/task3.cs
@@ -5,6 +5,16 @@ namespace task2
     {
         public static void Main(String[] args)
         {
+            /*// test scenarios
+            n=2; x=3
+            result: res1=2; res2=9;
+            n=5; x=2
+            result: res1=120; res2=32;
+            n=6; x=10
+            result: res1=720; res2=1000000;
+            n=12; x=14
+            result: res1=479001600; res2=344068096
+            */
             int n, x, res1, res2;
             Console.WriteLine(res1);
             Console.WriteLine(res2);
