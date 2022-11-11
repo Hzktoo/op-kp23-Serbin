f7f05189dfe3df1adad12166ff74340f11fe8ee7
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-11 19:33:14 +0200

    task_1_second_commit

--- sem_1_lab_1/task1.cs
+++ sem_1_lab_1/task1.cs
@@ -5,7 +5,17 @@ namespace task1
     {
         public static void Main(String[] args)
         {
-            double x0, xk, a, b, y;
+            /*// test scenarios
+        x0=5.3; xk=10.3; k=19
+        result: y0=150.36584953882053; yk=1094.7762766061146;
+        x0=2; xk=8; k=3
+        result: y0=9.36267736213967; yk=514.3373327533212
+        x0=14.3; xk=21.8; k=14
+        result: y0=2926.1917649000143; yk=10362.093640265355
+        x0=109; xk=1; k=54
+        result: y0=1295030.754318801; yk=27.801649381175796
+        */
+            double x0, xk, k, a, b, y;
             Console.WriteLine(y);
             }
         }
