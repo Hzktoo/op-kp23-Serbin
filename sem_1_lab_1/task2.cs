c3402d32dcb7a4b40075063cd75d9f90fc0d8707
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-11 20:25:39 +0200

    task_2_second_commit

--- sem_1_lab_1/task2.cs
+++ sem_1_lab_1/task2.cs
@@ -5,6 +5,18 @@ namespace task2
     {
         public static void Main(String[] args)
         {
+         /*// test scenarios
+         n=3
+         result: number n is prime;
+         n=18
+         result: number n is not prime;
+         n=-643
+         result: number n is not prime;
+         n=248
+         result: number n is not prime;
+         n=347
+         result: number n is prime.
+         */
             int n;
             Console.WriteLine("number n is prime");
             Console.WriteLine("number n is not prime");
