08154cf5190bd1bad271d6c5e3022d2abe10ee5a
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-12 13:08:53 +0200

    task_4_first_commit

---
+++ sem_1_lab_1/task4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace task4
+{
+    class Programm
+    {
+        public static void Main(String[] args)
+        {
+            double x;
+            int a;
+            double res;
+            Console.WriteLine(res);
+        }
+    }
+}
