53100e8f3a09a27ff5e256be883047d466219b48
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-11-12 13:52:45 +0200

    task_4_second_commit

--- sem_1_lab_1/task4.cs
+++ sem_1_lab_1/task4.cs
@@ -5,6 +5,21 @@ namespace task4
     {
         public static void Main(String[] args)
         {
+         /*// test scenarios
+         x=2; a=6
+         result: res=0.9092974278256817
+         x=3; a=10
+         result: res=0.1411200080598672
+         x=4; a=2
+         result: res=-0.7568024953079282
+         x=12; a=11
+         result: res=-0.53657291800004349
+         x=-8; a=4
+         result: res=-0.9893582466233818
+         x=5; a=2
+         result: res=-0.9589242746631385
+         */
+         
             double x;
             int a;
             double res;
