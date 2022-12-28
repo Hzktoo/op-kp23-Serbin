55c62410da87626013d6e186493eda80297edf5d
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2022-12-28 23:15:27 +0200

    second_commit_of_task

--- sem_1_lab_2/percolation.cs
+++ sem_1_lab_2/percolation.cs
@@ -2,7 +2,52 @@
 
 class Percolation
 {
-
+    // test cases
+    static bool testisOpen()
+    {
+        int[,] testgrid1 = { { 0, 2, 0, 2 }, { 1, 2, 1, 2 }, { 1, 1, 2, 1 }, { 2, 2, 1, 2 } };
+        int[,] testgrid2 = { { 0, 2, 2, 2 }, { 1, 1, 2, 1 }, { 1, 1, 1, 2 }, { 1, 1, 2, 2 } };
+        int[,] testgrid3 = { { 0, 0, 2, 0 }, { 1, 2, 2, 1 }, { 1, 2, 2, 1 }, { 1, 2, 1, 1 } };
+        bool testisOpen = true;
+        if (testisOpen != isOpen(1, 1, testgrid1) || testisOpen != isOpen(1,1, testgrid2) || testisOpen != isOpen(1, 1, testgrid3))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    static bool testisFull()
+    {
+        int[,] testgrid1 = { { 0, 2, 0, 2 }, { 1, 2, 1, 2 }, { 1, 1, 2, 1 }, { 2, 2, 1, 2 } };
+        int[,] testgrid2 = { { 0, 2, 2, 2 }, { 1, 1, 2, 1 }, { 1, 1, 1, 2 }, { 1, 1, 2, 2 } };
+        int[,] testgrid3 = { { 0, 0, 2, 0 }, { 1, 2, 2, 1 }, { 1, 2, 2, 1 }, { 1, 2, 1, 1 } };
+        bool testisFull = true;
+        if (testisFull != isFull(1, 1, testgrid1) || testisFull != isFull(1, 1, testgrid2) || testisFull != isFull(1, 1, testgrid3))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    static bool testpercolates()
+    {
+        int[,] testgrid1 = { { 0, 2, 0, 2 }, { 1, 2, 1, 2 }, { 1, 1, 2, 1 }, { 2, 2, 1, 2 } };
+        int[,] testgrid2 = { { 0, 2, 2, 2 }, { 1, 1, 2, 1 }, { 1, 1, 1, 2 }, { 1, 1, 2, 2 } };
+        int[,] testgrid3 = { { 0, 0, 2, 0 }, { 1, 2, 2, 1 }, { 1, 2, 2, 1 }, { 1, 2, 1, 1 } };
+        bool testpercolates = true;
+        if (testpercolates != percolates(testgrid1) || testpercolates != percolates(testgrid2) || testpercolates != percolates(testgrid3))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     // creates n-by-n grid, with all sites initially blocked
     static int [, ] init(int n)
     {
