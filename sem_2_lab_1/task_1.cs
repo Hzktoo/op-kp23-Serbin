9c67f763398e4b52bc6e0da8e22eb029a0136afe
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-06 11:47:58 +0200

    task_1_first_commit

---
+++ sem_2_lab_1/task_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = "D:\\KPI\\Programming\\";
+            string fileName = "Input.txt";
+            SaveTextToFile(folder, fileName);
+            LoadTextFromFile(folder, fileName);
+        }
+        static void SaveTextToFile(string folder, string fileName)
+        {
+            folder += fileName;
+            StreamWriter writedline = new StreamWriter(folder);
+        }
+        static void LoadTextFromFile(string folder, string fileName)
+        {
+            folder += fileName;
+            string line;
+            StreamReader writedline = new StreamReader(folder);
+        }
+    }
+}
