7ae149f88ac79f661c9461af4f5a6e154765029a
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-09 23:02:28 +0200

    task_2_first_commit

---
+++ sem_2_lab_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input;
+        string[] numbers = input.Split(' ');
+        string filePath = @"D:\KPI\Programming\random_numbers.txt";
+        StreamWriter writer = new StreamWriter(filePath);
+        double maxNumber = double.MinValue;
+        string maxFilePath = @"D:\KPI\Programming\max.txt";
+        StreamWriter maxWriter = new StreamWriter(maxFilePath);
+    }
+}
