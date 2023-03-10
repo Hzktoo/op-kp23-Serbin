be3c5fa8f033fc37344688da6d1cd42cf1bb494d
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 17:50:20 +0200

    task_4_first_commit

---
+++ sem_2_lab_1/task_4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "D:\\KPI\\students.csv";
+            List<string> students = ReadCSVFile(PATH);
+            List<string> badStudents = SearchBadStudents(students);
+        }
+        static List<string> SplitLine(string line, char spliter)
+        {
+            List<string> numbers = new List<string>();
+            int step = 0;
+            string number = "";
+            return numbers;
+        }
+
+        static List<string> ReadCSVFile(string path)
+        {
+            List<string> strings = new List<string>();
+            string line;
+            return strings;
+        }
+        static List<string> SearchBadStudents(List<string> students)
+        {
+            List<string> badStudents = new List<string>();
+            return badStudents;
+        }
+
+
+    }
+}
