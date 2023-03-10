e7219dcbda49e31dd2cc1017ce25d4cdec1bb152
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 14:36:00 +0200

    task_3_first_commit

---
+++ sem_2_lab_1/task_3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string inputFile = @"D:\KPI\Programming\Englishwords.txt";
+        string outputFile = @"D:\KPI\Programming\SortedWords.txt";
+        string[] lines = File.ReadAllLines(inputFile);
+        File.WriteAllLines(outputFile, lines);
+    }
+}
