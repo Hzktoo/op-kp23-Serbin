355a9d043076a77ceaba7e47d34620b34192c067
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 14:44:58 +0200

    task_3_third_commit

--- sem_2_lab_1/task_3.cs
+++ sem_2_lab_1/task_3.cs
@@ -43,6 +43,26 @@ class Program
         string inputFile = @"D:\KPI\Programming\Englishwords.txt";
         string outputFile = @"D:\KPI\Programming\SortedWords.txt";
         string[] lines = File.ReadAllLines(inputFile);
+        if (lines.Length > 40)
+        {
+            Console.WriteLine("Error: File contains more than 40 lines.");
+            return;
+        }
+        foreach (string line in lines)
+        {
+            if (line.Split(' ').Length > 2)
+            {
+                Console.WriteLine("Error: Line contains more than one word.");
+                return;
+            }
+            if (line.Length > 80)
+            {
+                Console.WriteLine("Error: Word exceeds 80 characters.");
+                return;
+            }
+        }
+        Array.Sort(lines);
         File.WriteAllLines(outputFile, lines);
+        Console.WriteLine("Words sorted and written to file.");
     }
 }
