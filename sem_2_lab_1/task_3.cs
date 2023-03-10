79ee84c1cb44697f0a36b28bdc143fea8b8abf04
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 14:43:59 +0200

    task_3_second_commit

--- sem_2_lab_1/task_3.cs
+++ sem_2_lab_1/task_3.cs
@@ -4,6 +4,40 @@ using System.Linq;
 
 class Program
 {
+       /*
+     Test scenariouses
+     Test 1:
+     Input: yes
+            no
+            KPI
+            whynotoke
+            please
+            go
+            within
+            computer
+            programing
+     Output: computer
+            go 
+            KPI
+            no
+            please
+            programing
+            whynotoke
+            within
+            yes
+     Test 2:
+     Input:  yes
+             no
+             KPI
+             whynotoke
+             please
+             go away
+             within
+             computer
+             programing
+     Output: Error: line contains more than one word
+     
+     */
     static void Main()
     {
         string inputFile = @"D:\KPI\Programming\Englishwords.txt";
