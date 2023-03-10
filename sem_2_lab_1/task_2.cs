d352415fb0a8542a33e0b2da38cf852acfd5ab5b
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 14:32:35 +0200

    task_2_second_commit

--- sem_2_lab_1/task_2.cs
+++ sem_2_lab_1/task_2.cs
@@ -2,7 +2,20 @@
 using System.IO;
 class Program
 {
-    
+    /*
+    Test scenariouses
+    Test 1:
+    input: 2, 10, 32, 24, 5, 8, 198, 64, 12, 43, 12, 15, 13, 76, 32
+    Output: 2, 10, 32, 24, 5, 8, 198, 64, 12, 43, 12, 15, 13, 76, 32
+            max: 198;
+    Test 2: 
+    Input: 2, 10, 32, 24, 5, 8, 198, 64, 12, 43, 12, 15, 13, 76
+    Output: Error: you must enter exactly 15 numbers;
+    Test 3:
+    Input: 3, 6, 8, 10, 12, 14, 16, 20, 46, 72, 11, 8, 5, 65, 42
+    Output: 3, 6, 8, 10, 12, 14, 16, 20, 46, 72, 11, 8, 5, 65, 42
+            max: 72
+    */
     static void Main(string[] args)
     {
         string input;
