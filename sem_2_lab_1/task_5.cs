d7606a75bf070e5431fe598f13060f482950a91e
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 18:02:17 +0200

    task_5_first_commit

---
+++ sem_2_lab_1/task_5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"D:\KPI\Programming\calculating.txt";
+        Dictionary<string, int> wordCounts = CountWordsInFile(filePath);
+        PrintWordCounts(wordCounts);
+        Console.ReadLine();
+    }
+    static Dictionary<string, int> CountWordsInFile(string filePath)
+    {
+        string fileContent = File.ReadAllText(filePath);
+        string[] words = fileContent.Split(new[] { ' ', '\t', '\n', '\r' },
+                                           StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+        return wordCounts;
+    }
+    static void PrintWordCounts(Dictionary<string, int> wordCounts)
+    {
+ 
+    }
+}
