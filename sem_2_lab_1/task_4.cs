c6a904f32796e88ff317b35d27640b92d90a24d1
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-03-10 17:58:13 +0200

    task_4_second_commit

--- sem_2_lab_1/task_4.cs
+++ sem_2_lab_1/task_4.cs
@@ -5,6 +5,41 @@ namespace Task4
 {
     class Program
     {
+        /*
+     Test scenariouses
+     Test 1:
+     Input:
+     First name 1	Last name 1	75
+     First name 2	Last name 2	24
+     First name 3	Last name 3	60
+     First name 4	Last name 4	100
+     First name 5	Last name 5	89
+     First name 6	Last name 6	50
+     First name 7	Last name 7	0
+     First name 8	Last name 8	19
+     First name 9	Last name 9	45
+     First name 10	Last name 10 71
+     Output:
+     First name 2	Last name 2	24
+     First name 6	Last name 6	50
+     First name 7	Last name 7	0
+     First name 8	Last name 8	19
+     First name 9	Last name 9	45;
+     Test 2:
+     Input: 
+     First name 1	Last name 1	75
+     First name 2	Last name 2	61
+     First name 3	Last name 3	60
+     First name 4	Last name 4	100
+     First name 5	Last name 5	89
+     First name 6	Last name 6	99
+     First name 7	Last name 7	67
+     First name 8	Last name 8	84
+     First name 9	Last name 9	100
+     First name 10	Last name 10 71
+     Output:
+     There is no bad students
+     */
         static void Main(string[] args)
         {
             const string PATH = "D:\\KPI\\students.csv";
