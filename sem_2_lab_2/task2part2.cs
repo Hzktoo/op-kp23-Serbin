b63879a17bc3e371bd90d04c7169448977b1f67e
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-04-12 11:43:30 +0300

    task2.2_third_commit

---
+++ sem_2_lab_2/task2part2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+abstract class Vessel
+{
+    public abstract void PrepareToMovement();
+    public abstract void Move();
+}
+
+class SailingVessel : Vessel
+{
+    public override void PrepareToMovement()
+    {
+        Console.WriteLine("Preparing the sailing vessel to move.");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Moving the sailing vessel.");
+    }
+}
+
+class Submarine : Vessel
+{
+    public override void PrepareToMovement()
+    {
+        Console.WriteLine("Preparing the submarine to move.");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Moving the submarine.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vessel vessel1 = new SailingVessel();
+        Vessel vessel2 = new Submarine();
+
+        vessel1.PrepareToMovement();
+        vessel1.Move();
+
+        vessel2.PrepareToMovement();
+        vessel2.Move();
+    }
+}
