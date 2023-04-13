f4d2cedba00cf1f29a5bdbc9a2734df983e01f4d
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-04-13 21:43:28 +0300

    task_3_third_commit

--- sem_2_lab_2/task3.cs
+++ sem_2_lab_2/task3.cs
@@ -26,18 +26,41 @@ public class Vector
     public static int operator +(Vector a, Vector b)
     {
         int res = 0;
+        int[] result = new int[a.Elements.Length];
+        for (int i = 0; i < a.Elements.Length; i++)
+        {
+            if (a.Elements[i] < 0)
+            {
+                res += a.Elements[i];
+            }
+            if (b.Elements[i] < 0)
+            {
+                res += b.Elements[i];
+            }
+        }
         return res;
     }
 
     public static Vector operator *(Vector a, Vector b)
     {
         int[] result = new int[a.Elements.Length];
+        for (int i = 1; i < a.Elements.Length; i += 2)
+        {
+            result[i] = a.Elements[i] * b.Elements[i];
+        }
         return new Vector(result);
     }
 
     public static int CountZeroElements(Vector a, Vector b)
     {
         int count = 0;
+        for (int i = 0; i < a.Elements.Length; i++)
+        {
+            if (a.Elements[i] == 0 || b.Elements[i] == 0)
+            {
+                count++;
+            }
+        }
         return count;
     }
 }
@@ -45,21 +68,34 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Enter the number of elements in the vectors:");
         int n = int.Parse(Console.ReadLine());
+
         int[] elements1 = new int[n];
         int[] elements2 = new int[n];
+
+        Console.WriteLine("Enter the elements of the first vector:");
         for (int i = 0; i < n; i++)
         {
             elements1[i] = int.Parse(Console.ReadLine());
         }
+
+        Console.WriteLine("Enter the elements of the second vector:");
         for (int i = 0; i < n; i++)
         {
             elements2[i] = int.Parse(Console.ReadLine());
         }
+
         Vector v1 = new Vector(elements1);
         Vector v2 = new Vector(elements2);
-        int sum;
-        Vector product;
-        int countZeroElements;
+
+        int sum = v1 + v2;
+        Console.WriteLine("Sum of negative elements: " + string.Join(", ", sum));
+
+        Vector product = v1 * v2;
+        Console.WriteLine("Product of elements with even indices: " + string.Join(", ", product.Elements));
+
+        int countZeroElements = Vector.CountZeroElements(v1, v2);
+        Console.WriteLine("Number of zero elements: " + countZeroElements);
     }
 }
