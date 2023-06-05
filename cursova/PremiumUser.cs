c83cd28a88b9e01f2e114afb7791299ac0893be4
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-06-05 23:18:25 +0300

    Add files via upload

---
+++ cursova/PremiumUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaForm
+{
+    public class PremiumUser : User
+    {
+        private static bool _isPremium = false;
+
+        public PremiumUser(string username, string email, string password)
+            : base(username, email, password)
+        {
+        }
+
+        public static bool IsPremiumUser()
+        {
+            return _isPremium;
+        }
+
+        public static void SetPremiumUser()
+        {
+            _isPremium = true;
+        }
+    }
+}
