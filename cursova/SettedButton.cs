b368d96d18705143c32b85f0666a2d896b2ff551
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-06-05 23:20:16 +0300

    Add files via upload

---
+++ cursova/SettedButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaForm
+{
+    public class SettedButton : Button
+    {
+        public SettedButton()
+        {
+            Size = new Size(75, 38);
+            BackColor = Color.DeepSkyBlue;
+        }
+    }
+}
