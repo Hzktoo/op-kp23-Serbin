23f842e1a90f3f840606df2d33ba1cb293c171d9
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-06-05 23:21:44 +0300

    Add files via upload

---
+++ cursova/UserManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaForm
+{
+    public abstract class UserManager
+    {
+        protected string UsersFilePath = "D:\\users.txt";
+
+        public abstract bool IsUserLoggedIn();
+        public abstract void RegisterUser();
+        public abstract void Login();
+        public abstract string GetLoggedInUserName();
+        public abstract void Logout();
+
+        protected bool IsUserRegistered(string email)
+        {
+            if (File.Exists(UsersFilePath))
+            {
+                string[] lines = File.ReadAllLines(UsersFilePath);
+                foreach (string line in lines)
+                {
+                    string[] userInfo = line.Split(',');
+
+                    if (userInfo.Length >= 3 && userInfo[1] == email)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
