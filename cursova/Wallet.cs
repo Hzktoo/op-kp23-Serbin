4bb1de630e19ca4fda3b8be063865d32c050bd86
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-06-05 23:22:01 +0300

    Add files via upload

---
+++ cursova/Wallet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaForm
+{
+    public class Wallet
+    {
+        public decimal BitcoinBalance { get; set; }
+        public decimal EthereumBalance { get; set; }
+        public decimal ShibDogeBalance { get; set; }
+
+        public Wallet(decimal bitcoinBalance, decimal ethereumBalance, decimal shibDogeBalance)
+        {
+            BitcoinBalance = bitcoinBalance;
+            EthereumBalance = ethereumBalance;
+            ShibDogeBalance = shibDogeBalance;
+        }
+    }
+}
