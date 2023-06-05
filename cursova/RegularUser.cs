886efc162388d39038b6858515fb5c5147e2ec18
Hzktoo <114067891+Hzktoo@users.noreply.github.com>
2023-06-05 23:19:14 +0300

    Add files via upload

---
+++ cursova/RegularUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaForm
+{
+    public class RegularUser : User
+    {
+        public RegularUser(string username, string email, string password)
+            : base(username, email, password)
+        {
+        }
+    }
+}
